32cd50641fd071f569bdf3bc3a42750dd13a336c
gatterD <152513558+gatterD@users.noreply.github.com>
2025-02-11 10:17:49 +0300

    Migrated the project from Flask sqlalchemy to Node.js + NestJS + WebSockets to improve project performance and scalability in the future, as well as generate real-time correspondence.

--- lib/models/AutorisationHelper.dart
+++ lib/models/AutorisationHelper.dart
@@ -2,29 +2,49 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:smarttalk/models/User.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   final String baseUrl = dotenv.get('BASEURL');
 
   Future<bool> register(User user) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/register'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(user.toJson()),
-    );
-    return response.statusCode == 201;
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/register'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(user.toJson()),
+      );
+
+      if (response.statusCode == 201) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
   }
 
-  Future<String?> login(User user) async {
+  Future<bool> login(String username, String password) async {
     final response = await http.post(
       Uri.parse('$baseUrl/login'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(user.toJson()),
+      body: jsonEncode({'username': username, 'password': password}),
     );
+
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      return data['access_token'];
+      final token = data['token']; // Проверить, не null ли это поле
+
+      if (token == null) {
+        throw Exception("Ошибка: сервер не вернул токен");
+      }
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', token);
+      return true;
+    } else {
+      return false;
     }
-    return null;
   }
 }
